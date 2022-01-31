48ff267fa42f18b54f0d05a947df736e4c3ed652
aryams3025 <80573229+aryams3025@users.noreply.github.com>
2022-01-31 18:42:15 +0530

    Update pgm16.php

--- pgm16.php
+++ pgm16.php
@@ -1,4 +1,14 @@
 <?php
+$conn=mysqli_connect("localhost","root","","student");
+if($conn)
+{
+    echo("successfully connected");
+echo"<br>";
+}
+else{
+    echo("error");
+    echo"<br>";
+}
 if(isset($_POST['submit']))
 {
     $username=$_POST['username'];
@@ -9,46 +19,45 @@ if(isset($_POST['submit']))
     $lowercase=preg_match('@[a-z]@', $password);
     $number=preg_match('@[0-9]@', $password);
     $number1=preg_match('@[0-9]@', $phonenumber);
-    $conn=mysqli_connect("localhost","root","","login_dbb");
+    
+$flag=0;
 
 
-if($conn)
-{
-    echo("successfully connected");
-echo"<br>";
-}
-else{
-    echo("error");
-    echo"<br>";
-}
 
 
 if($username==""){
 echo "please fill the username <br>";
+$flag=1;
 }
 if( $password=="" && strlen($password) < 8){
 echo "Password should be at least 8 characters in length <br>";
+$flag=1;
 }
 else if( !$uppercase || !$lowercase || !$number ) {
 echo "password should include at least one upper case letter, one number, and one special character.<br>";
+$flag=1;
 }
 else{
 echo "Strong password.<br>";
 }
 if($emailid==""){
 echo "please fill the email id<br>";
+$flag=1;
 }
 if($phonenumber==""){
 echo " please fill the phone number<br>";
+$flag=1;
 }
 else if(strlen($phonenumber)<10){
 echo "please fill correct phone number <br> "; 
+$flag=1;
 }
 else{
 echo "phone number is correct<br>";
 }
-
-$query="INSERT INTO tb_login(username,password)VALUES('{$username}','{$password}')";
+if($flag==0)
+{
+$query="INSERT INTO stud(username,password,phonenumber,emailid)VALUES('{$username}','{$password}','{$phonenumber}','{$emailid}')";
 if(mysqli_query($conn,$query))
 {
     echo("successfully inserted");
@@ -58,13 +67,8 @@ else{
     echo("insertion failed");
     echo"<br>";
 }
-$search="SELECT * FROM tb_login";
-$data=mysqli_query($conn,$search);
-while($res=mysqli_fetch_assoc($data))
-{
-echo $res['username'],' ';
-echo $res['password'],' ';
 }
+
 }
 ?>
 <html>
@@ -78,6 +82,38 @@ echo $res['password'],' ';
     emailid:<input type="text" name="emailid"><br><br>
     <input type="submit" name="submit">
 </form>
+<table border=1>
+    <tr>
+        <th>username</th>
+        <th>password</th>
+        <th>phone number</th>
+        <th>email id</th>
+    </tr> 
+    <?php
+    $search="SELECT * FROM stud";
+    $data=mysqli_query($conn,$search);
+    
+    
+    while($res=mysqli_fetch_assoc($data))
+    {?>
+    <tr>
+    <td>
+        <?php echo $res['username'];?>
+    </td>
+    <td>
+        <?php echo $res['password'];?>
+    </td>
+    <td>
+        <?php echo $res['phonenumber'];?>
+    </td>
+    <td>
+        <?php echo $res['emailid'];?>
+    </td>
+    </tr>
+    
+    <?php
+    }
+    ?>   
 </center> 
 </body>
 </html>
