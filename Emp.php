1228d4e23e753965b1b82e835fa0444a11994813
aryams3025 <80573229+aryams3025@users.noreply.github.com>
2022-02-03 11:10:06 +0530

    Add files via upload

---
+++ Emp.php
@@ -0,0 +1,86 @@
+<?php
+$conn=mysqli_connect("localhost","root","","Employee");
+if($conn)
+{
+    echo("successfully connected");
+echo"<br>";
+}
+else{
+    echo("error");
+    echo"<br>";
+}
+if(isset($_POST['submit']))
+{ 
+    $empid=$_POST['Emp_id'];
+    $empname=$_POST['Emp_name'];
+    $jobname=$_POST['Job_name'];
+    $managerid=$_POST['Manager_id'];
+    $salary=$_POST['Salary'];
+    $number=preg_match('@[0-9]@', $salary);
+    $flag=0;
+if($empname==""){
+echo "please fill the empname <br>";
+$flag=1;
+}
+if( $salary=="" ){
+echo "Please fill the salary <br>";
+$flag=1;
+}
+else{
+echo "Salary correct<br>";
+}
+if($flag==0)
+{
+$query="INSERT INTO emp(Emp_id,Emp_name,Job_name,Manager_id,Salary)VALUES('{$empid}','{$empname}','{$jobname}','{$managerid}','{$salary}')";
+if(mysqli_query($conn,$query))
+{
+    echo("successfully inserted");
+    echo "<br>";
+}
+else{
+    echo("insertion failed");
+    echo"<br>";
+}
+}
+
+}
+?>
+<html>
+<body bgcolor="pink">
+<center>
+<form action="" method="POST">
+    <h1>EMPLOYEE DETAILS</h1>
+    Emp_id:<input type="text" name="Emp_id"><br><br>
+    Emp_name:<input type="text" name="Emp_name"><br><br>
+    Job_name:<input type="text" name="Job_name"><br><br>
+    Manager_id:<input type="text" name="Manager_id"><br><br>
+    Salary:<input type="text" name="Salary"><br><br>
+    <input type="submit" name="submit" value="SUBMIT">
+</form>
+<table border=1>
+    <tr>
+        <th>Emp_name</th>
+        <th>Salary</th>
+    </tr> 
+    <?php
+    $search="SELECT Emp_name,Salary FROM emp WHERE Salary>35000";
+    $data=mysqli_query($conn,$search);
+    
+    
+    while($res=mysqli_fetch_assoc($data))
+    {?>
+    <tr>
+    <td>
+        <?php echo $res['Emp_name'];?>
+    </td>
+    <td>
+        <?php echo $res['Salary'];?>
+    </td>
+    </tr>
+    
+    <?php
+    }
+    ?>   
+</center> 
+</body>
+</html>
